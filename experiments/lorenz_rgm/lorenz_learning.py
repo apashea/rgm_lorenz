be41c8f61837045956ff96d434882b74064d9f62
Andrew Pashea <45848237+apashea@users.noreply.github.com>
2026-03-17 18:01:41 -0500

    Update lorenz_learning.py

--- experiments/lorenz_rgm/lorenz_learning.py
+++ experiments/lorenz_rgm/lorenz_learning.py
@@ -346,7 +346,9 @@ def train_lorenz_rgm(
     params = initial_params
 
     for epoch in range(num_epochs):
-        for _ in range(num_sequences_per_epoch):
+        #print(f"train_lorenz_rgm(): Epoch {epoch+1}/{num_epochs}")
+        for seq_i in range(num_sequences_per_epoch):
+            print(f"train_lorenz_rgm(): Epoch {epoch+1}/{num_epochs} - Sequence {seq_i}/{num_sequences_per_epoch}")
             lorenz_data_dict = build_data_fn()
 
             hierarchy = build_lorenz_hierarchy_from_params(
